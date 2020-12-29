91fd245b899e4fa61173824a419e6319b61568d1
Rizal301 <53280611+Rizal301@users.noreply.github.com>
2020-12-30 06:09:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://glitch.com/edit/#!/lilybot-music"]
+const urls = ["https://lilybot-music.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
